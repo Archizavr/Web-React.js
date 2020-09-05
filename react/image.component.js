08da1fb1659cf7da38bb8a67e1794fcdde05bf91
Archizavr <47066517+Archizavr@users.noreply.github.com>
2020-09-05 19:33:19 +0300

    final commit

--- react/image.component.js
+++ react/image.component.js
@@ -1,9 +1,16 @@
 class Image extends React.Component {
+    enlarge () {
+        window.open(this.props.imageSrc);
+    }
+
     render() {
-        let html = e("div", null, "test");
-        let attributes = {className: "image", style: {
-            backgroundImage: "url(resources/images/forest.jpg)"
-        } 
+        let html = e("div", null, this.props.desc);
+        let attributes = {className: "item image", 
+        style: {
+            backgroundImage: "url(" + this.props.imageSrc + ")"
+            
+        },
+        onClick: this.enlarge.bind(this)         
     };
         return e("div",attributes, html);
     }
